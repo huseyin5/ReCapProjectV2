a2ca4f6531c00b0d2a3152f8c66de3db0985bb2a
huseyin5 <395273@ogr.ktu.edu.tr>
2023-08-20 15:39:29 +0300

    update core

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        private IBrandDal _iBrandDal;
+
+        public BrandManager(IBrandDal iBrandDal)
+        {
+            _iBrandDal = iBrandDal;
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _iBrandDal.GetAll();
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,8 +1,24 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntitiyFramework;
 
-CarManager carManager = new CarManager(new EfCarDal());
-foreach (var car in carManager.GetAll())
+//CarTest();
+
+BrandTest();
+
+static void CarTest()
 {
-    Console.WriteLine(car.Description);
+    CarManager carManager = new CarManager(new EfCarDal());
+    foreach (var car in carManager.GetAll())
+    {
+        Console.WriteLine("{0} - {1} TL", car.Description, car.DailyPrice);
+    }
+}
+
+static void BrandTest()
+{
+    BrandManager brandManager = new BrandManager(new EfBrandDal());
+    foreach (var brand in brandManager.GetAll())
+    {
+        Console.WriteLine(brand.BrandName);
+    }
 }
